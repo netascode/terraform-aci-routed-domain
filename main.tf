4e4e407ada40ee15208565133bd91e15454bc446
danischm <danischm@cisco.com>
2021-10-11 20:25:49 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aci_rest" "l3extDomP" {
 }
 
 resource "aci_rest" "infraRsVlanNs" {
-  dn         = "${aci_rest.l3extDomP.id}/rsvlanNs"
+  dn         = "${aci_rest.l3extDomP.dn}/rsvlanNs"
   class_name = "infraRsVlanNs"
   content = {
     tDn = "uni/infra/vlanns-[${var.vlan_pool}]-${var.vlan_pool_allocation}"
